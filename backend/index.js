df142589e30b78c8b01b6814f9c314bdc660babd
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2023-06-30 07:22:33 -0300

    Integracion del SDK de mercado pago en el backend.

--- backend/index.js
+++ backend/index.js
@@ -11,3 +11,30 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor Express escuchando en el puerto ${port}`);
 });
+
+// SDK de Mercado Pago
+const mercadopago = require("mercadopago");
+// Agrega credenciales
+mercadopago.configure({
+  access_token: "PROD_ACCESS_TOKEN",
+});
+
+// Crea un objeto de preferencia
+let preference = {
+  items: [
+    {
+      title: "SLAT",
+      unit_price: 500,
+      quantity: 1,
+    },
+  ],
+};
+
+mercadopago.preferences
+  .create(preference)
+  .then(function (response) {
+    // En esta instancia deberás asignar el valor dentro de response.body.id por el ID de preferencia solicitado en el siguiente paso
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
