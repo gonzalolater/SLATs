568778bd27ed96fe1753c5c5538b346c65fd3faa
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2023-09-09 17:59:50 -0300

    Update Slat-Wmatic-Wusdc.sol - > Burneable

--- contract/contracts/src/Polygon/Matic/Slat-Wmatic-Wusdc.sol
+++ contract/contracts/src/Polygon/Matic/Slat-Wmatic-Wusdc.sol
@@ -1,17 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SLATs is ERC721, Ownable {
-    constructor() ERC721("MyNFT", "MNFT") {}
+contract SLATs is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("SLATs", "SLT") {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
 
-    function mint(address to, uint256 tokenId) external onlyOwner {
-        _mint(to, tokenId);
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
-
 // En este ejemplo, el contrato MyNFT hereda de los contratos ERC721 y Ownable de OpenZeppelin. ERC721 es la implementación estándar para contratos de tokens no fungibles (NFT) en Ethereum, mientras que Ownable proporciona una funcionalidad básica de control de acceso para la propiedad del contrato.
 // El constructor del contrato se encarga de establecer el nombre y el símbolo del NFT utilizando la función ERC721 y sus parámetros "MyNFT" y "MNFT", respectivamente.
 // La función mint permite al propietario del contrato (el que tiene el rol de "owner") crear nuevos NFTs y asignarlos a una dirección específica. En este ejemplo, la función recibe la dirección del destinatario (to) y el ID del token (tokenId), y utiliza la función _mint heredada de ERC721 para realizar la creación y asignación del NFT.
