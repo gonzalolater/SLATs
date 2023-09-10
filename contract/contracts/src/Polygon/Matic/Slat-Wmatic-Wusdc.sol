cd3ab71a23af136c16b3fc1982eea598a3225655
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2023-09-10 18:33:58 -0300

    Update Slat-Wmatic-Wusdc.sol -> premint - > lf option to Wrap

--- contract/contracts/src/Polygon/Matic/Slat-Wmatic-Wusdc.sol
+++ contract/contracts/src/Polygon/Matic/Slat-Wmatic-Wusdc.sol
@@ -14,6 +14,8 @@ contract SLATs is ERC20, ERC20Burnable, Ownable {
         _mint(to, amount);
     }
 }
+
+// Premint 10.000 tokens, buscando mejores opciones para wrapear el matic y conservar el matic real dentro de esta cuenta, osea los titulos de propiedad.
 // En este ejemplo, el contrato MyNFT hereda de los contratos ERC721 y Ownable de OpenZeppelin. ERC721 es la implementación estándar para contratos de tokens no fungibles (NFT) en Ethereum, mientras que Ownable proporciona una funcionalidad básica de control de acceso para la propiedad del contrato.
 // El constructor del contrato se encarga de establecer el nombre y el símbolo del NFT utilizando la función ERC721 y sus parámetros "MyNFT" y "MNFT", respectivamente.
 // La función mint permite al propietario del contrato (el que tiene el rol de "owner") crear nuevos NFTs y asignarlos a una dirección específica. En este ejemplo, la función recibe la dirección del destinatario (to) y el ID del token (tokenId), y utiliza la función _mint heredada de ERC721 para realizar la creación y asignación del NFT.
